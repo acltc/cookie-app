b86daa8e062b0f7e4b8796a685f372e29ca42e60
Jay Wengrow <jaywngrw@gmail.com>
2014-10-23 12:54:54 -0500

    moving cookie data to session

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,35 +1,35 @@
 class GamesController < ApplicationController
   def guess_a_number
-    if cookies[:secret_number].nil?
-      cookies[:secret_number] = rand(1..100)
+    if session[:secret_number].nil?
+      session[:secret_number] = rand(1..100)
     end
   end
 
   def submit_number
-    secret_number = cookies[:secret_number].to_i
+    secret_number = session[:secret_number].to_i
     
-    if cookies[:number_of_guesses]
-      cookies[:number_of_guesses] = cookies[:number_of_guesses].to_i + 1
+    if session[:number_of_guesses]
+      session[:number_of_guesses] = session[:number_of_guesses].to_i + 1
     else
-      cookies[:number_of_guesses] = 1
+      session[:number_of_guesses] = 1
     end
 
     if secret_number > params[:number].to_i
       flash[:warning] = "Too low! Guess higher."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
+      flash[:info] = "You have made #{session[:number_of_guesses]} guesses so far."
     elsif secret_number < params[:number].to_i
       flash[:warning] = @message = "Too high! Guess lower."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
+      flash[:info] = "You have made #{session[:number_of_guesses]} guesses so far."
     elsif secret_number == params[:number].to_i
-      flash[:success] = "You win! The correct number was indeed #{secret_number}! You got it in #{cookies[:number_of_guesses]} guesses!"
+      flash[:success] = "You win! The correct number was indeed #{secret_number}! You got it in #{session[:number_of_guesses]} guesses!"
     end
 
     redirect_to '/'
   end
 
   def reset
-    cookies[:secret_number] = rand(1..100)
-    cookies[:number_of_guesses] = 0
+    session[:secret_number] = rand(1..100)
+    session[:number_of_guesses] = 0
     flash[:success] = "New game! Begin playing."
     redirect_to '/'
   end
