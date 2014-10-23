b48b14a80d561cbfb16f6f47a24e557aa299850e
Jay Wengrow <jaywngrw@gmail.com>
2014-10-23 12:36:37 -0500

    computer now picks random number

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,8 +4,12 @@ class GamesController < ApplicationController
   end
 
   def submit_number
-    secret_number = 45
+    if cookies[:secret_number].nil?
+      cookies[:secret_number] = rand(1..100)
+    end
 
+    secret_number = cookies[:secret_number].to_i
+    
     if cookies[:number_of_guesses]
       cookies[:number_of_guesses] = cookies[:number_of_guesses].to_i + 1
     else
@@ -14,10 +18,10 @@ class GamesController < ApplicationController
 
     if secret_number > params[:number].to_i
       flash[:warning] = "Too low! Guess higher."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
     elsif secret_number < params[:number].to_i
       flash[:warning] = @message = "Too high! Guess lower."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
     elsif secret_number == params[:number].to_i
       flash[:success] = "You win! The correct number was indeed #{secret_number}!"
     end
