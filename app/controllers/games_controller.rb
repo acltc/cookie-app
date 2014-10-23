707ce9da87da1a835773874793e0145838ebcd56
Jay Wengrow <jaywngrw@gmail.com>
2014-10-23 12:27:39 -0500

    tracking number of guesses

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,10 +6,18 @@ class GamesController < ApplicationController
   def submit_number
     secret_number = 45
 
+    if cookies[:number_of_guesses]
+      cookies[:number_of_guesses] = cookies[:number_of_guesses].to_i + 1
+    else
+      cookies[:number_of_guesses] = 1
+    end
+
     if secret_number > params[:number].to_i
       flash[:warning] = "Too low! Guess higher."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
     elsif secret_number < params[:number].to_i
       flash[:warning] = @message = "Too high! Guess lower."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
     elsif secret_number == params[:number].to_i
       flash[:success] = "You win! The correct number was indeed #{secret_number}!"
     end
