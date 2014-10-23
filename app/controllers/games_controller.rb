8868ab66fc667ee136227cbe771341f987932b6e
Jay Wengrow <jaywngrw@gmail.com>
2014-10-23 12:34:24 -0500

    typo

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,10 +14,10 @@ class GamesController < ApplicationController
 
     if secret_number > params[:number].to_i
       flash[:warning] = "Too low! Guess higher."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
     elsif secret_number < params[:number].to_i
       flash[:warning] = @message = "Too high! Guess lower."
-      flash[:info] = "You have made #{cookies[:number_of_guesses]} so far."
+      flash[:info] = "You have made #{cookies[:number_of_guesses]} guesses so far."
     elsif secret_number == params[:number].to_i
       flash[:success] = "You win! The correct number was indeed #{secret_number}!"
     end
